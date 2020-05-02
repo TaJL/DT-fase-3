c4746858d6d5857a93d55dfc7cb6e3ce815274b9
VG <vengadoravg@gmail.com>
2020-05-02 13:59:28 -0400

    character movement

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class FollowingCamera : MonoBehaviour {
-  public float smoothTime = 1;
-  public Transform target;
-
-  Vector3 _velocity;
-
-  void Update () {
-    transform.position =
-      Vector3.SmoothDamp(transform.position, target.position,
-                         ref _velocity, smoothTime);
+public class FollowingCamera : SmoothFollow {
+  void OnEnable () {
+    this.target =
+      GameObject.FindWithTag("Player").GetComponent<PlayerControl>().cameraTarget;
   }
 }
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerControl : MonoBehaviour {
+  public float speed = 8;
+  public Transform controlPov;
+  public Transform cameraTarget;
+  public float maxCameraDistance = 2;
+  public Transform visuals;
+
+  Vector3 direction;
+
+  void FixedUpdate () {
+    direction = (controlPov.right * Input.GetAxisRaw("Horizontal") +
+                 controlPov.forward * Input.GetAxisRaw("Vertical"));
+
+    transform.position += direction * speed * Time.deltaTime;
+
+    if (direction != Vector3.zero) {
+      cameraTarget.localPosition = direction.normalized * maxCameraDistance;
+      if (direction.x != 0) {
+        visuals.localScale = new Vector3(Mathf.Sign(direction.x), 1, 1) *
+          visuals.transform.localScale.z;
+      }
+    }
+  }
+}
