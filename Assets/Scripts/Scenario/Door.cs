758a3a95fed920b17acbd62072da50fb2725746c
Sehgel <lucasemanuelgonzalez8@gmail.com>
2020-05-03 00:58:12 -0300

    Doors added as obstacle

--- Assets/Scripts/Scenario/Door.cs
+++ Assets/Scripts/Scenario/Door.cs
@@ -19,9 +19,11 @@ public class Door : MonoBehaviour
     [SerializeField]
     private float animation_time = 0.5f;
     private bool opened = false;
-    private Vector3 start;
-    private Vector3 end;
+    private Vector3 start_position;
+    private Vector3 end_position;
 
+    private Quaternion start_rotation;
+    private Quaternion end_rotation;
     private Transform physical_part;
 
 
@@ -29,8 +31,10 @@ public class Door : MonoBehaviour
     {
         //Setup
         physical_part = transform.FindChildWithTag("Start");
-        start = physical_part.position;
-        end = transform.FindChildWithTag("End").position;
+        start_position = physical_part.position;
+        start_rotation = physical_part.rotation;
+        end_position = transform.FindChildWithTag("End").position;
+        end_rotation = transform.FindChildWithTag("End").rotation;
     }
     void Start()
     {
@@ -51,7 +55,9 @@ public class Door : MonoBehaviour
     {
         //Determine start and end based on current state
         Vector3 start_pos = physical_part.position;
-        Vector3 end_pos = new_state ? end : start;
+        Quaternion start_rot = physical_part.rotation;
+        Vector3 end_pos = new_state ? end_position : start_position;
+        Quaternion end_rot = new_state ? end_rotation : start_rotation;
         //Toggle
         opened = new_state;
 
@@ -60,6 +66,7 @@ public class Door : MonoBehaviour
         {
             counter = Mathf.Clamp(counter + Time.deltaTime / animation_time, 0, 1);
             physical_part.position = Vector3.LerpUnclamped(start_pos, end_pos, curve.Evaluate(counter));
+            physical_part.rotation = Quaternion.LerpUnclamped(start_rot, end_rot, curve.Evaluate(counter));
             yield return null;
         }
     }
@@ -86,7 +93,9 @@ public class Door : MonoBehaviour
     {
         Gizmos.DrawWireSphere(transform.position, area_range);
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.FindChildWithTag("End").position, transform.FindChildWithTag("Start").GetComponent<MeshRenderer>().bounds.size);
+        Transform end_transform = transform.FindChildWithTag("End");
+        Gizmos.matrix = Matrix4x4.TRS(end_transform.position, end_transform.rotation, end_transform.localScale);
+        Gizmos.DrawWireCube(Vector3.zero, transform.FindChildWithTag("Start").GetComponent<MeshRenderer>().bounds.size);
     }
 #endif
 }
