5ef09675cf4c0f5bd60496578f304af0541b59d6
Tadeo <tadeo@ravegan.com>
2020-05-03 12:37:41 -0300

    basic enemy

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Tools;
+
+namespace Game {
+
+public class BasicEnemy : MonoBehaviour {
+	
+	public virtual StateMachine stateMachine = new StateMachine();
+	public virtual Player player;
+    public 
+
+	// -- //
+	
+	void Awake() {
+		stateMachine.AddState("Idle", fixedUpdate: Idle_FixedUpdate)
+			.To("Jumping", () => Input.GetKeyDown(KeyCode.Space), enter: Jumping_Enter, update: Jumping_Update, exit: Jumping_Exit)
+		.To("Idle", () => Input.GetKeyUp(KeyCode.Space))
+			.To("Crouching", () => Input.GetKey(KeyCode.DownArrow), enter: Crouching_Enter, update: Crouching_Update, exit: Crouching_Exit)
+		.To("Idle", () => !Input.GetKey(KeyCode.DownArrow))
+		.AnyState("Shooting", () => Input.GetKeyDown(KeyCode.A), enter: Enter_Shooting, update: Update_Shooting, exit: Exit_Shooting).Exit(() => Input.GetKeyUp(KeyCode.A))
+		;
+	}
+	
+	void Update() {
+		stateMachine.Update();
+	}
+	
+	void FixedUpdate() {
+		stateMachine.FixedUpdate();
+	}
+	
+	// -- //
+	
+	void Idle_Enter() {
+		Debug.Log("Enter to Idle");
+	}
+	
+	void Idle_Update() {
+		Debug.Log("I'm idling :)");
+	}
+	
+	void Idle_FixedUpdate() {
+		Debug.Log("Fixed idling");
+	}
+	
+	void Idle_Exit() {
+		Debug.Log("Exit from Idle");
+	}
+	
+	// -- //
+	
+	void Jumping_Enter() {
+		Debug.Log("JUMP !");
+	}
+	
+	void Jumping_Update() {
+		Debug.Log("Lol I'm in the air :]");
+	}
+	
+	void Jumping_Exit() {
+		Debug.Log("On the ground");
+	}
+	
+	// -- //
+	
+	void Crouching_Enter() {
+		Debug.Log("Chilling on the ground");
+	}
+	
+	void Crouching_Update() {
+		Debug.Log("Still chilling");
+	}
+	
+	void Crouching_Exit() {
+		Debug.Log("Enough chill");
+	}
+	
+	// -- //
+	
+	void Enter_Shooting() {
+		Debug.Log("Enter Shooting");
+	}
+	
+	void Update_Shooting() {
+		Debug.Log("Update Shooting");
+	}
+	
+	void Exit_Shooting() {
+		Debug.Log("Exit Shooting");
+	}
+	
+}
+
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,244 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Tools
+{
+    public delegate bool Condition();
+    public delegate void EnterState();
+    public delegate void UpdateState();
+    public delegate void FixedUpdateState();
+    public delegate void ExitState();
+
+    public class StateOutput
+    {
+        public Condition condition;
+        public State toState;
+    }
+
+    public class State
+    {
+        public string Name 
+        {
+            get 
+            { 
+                return name; 
+            }
+	    }
+        string name;
+        StateMachine stateMachine;
+        
+        protected EnterState enter;
+        protected UpdateState update;
+        protected FixedUpdateState fixedUpdate;
+        protected ExitState exit;
+        
+        protected List<StateOutput> outputStates = new List<StateOutput>();
+        
+        bool firstUpdate = true;
+	
+        protected void Enter()
+        {
+            if(firstUpdate)
+            {
+                firstUpdate = false;
+                if(enter != null)
+                {
+                    enter();
+                }
+            }
+        }
+	
+        protected void Exit()
+        {
+            firstUpdate = true;
+            if(exit != null)
+            {
+                exit();
+            }
+        }
+	
+        public State(StateMachine stateMachine, string name, EnterState enter, UpdateState update, FixedUpdateState fixedUpdate, ExitState exit)
+        {
+            this.stateMachine = stateMachine;
+            this.name        = name;
+            this.enter       = enter;
+            this.update      = update;
+            this.fixedUpdate = fixedUpdate;
+            this.exit        = exit;
+        }
+	
+        public State To(string name, Condition switchCondition,
+            EnterState enter = null, UpdateState update = null,
+            FixedUpdateState fixedUpdate = null, ExitState exit = null)
+        {
+            State stateToAdd = stateMachine.GetState(name);
+            if(stateToAdd == null)
+            {
+                stateToAdd = stateMachine.AddState(name, enter, update, fixedUpdate, exit);
+            }
+		
+            StateOutput output = new StateOutput();
+            output.condition   = switchCondition;
+            output.toState     = stateToAdd;
+		
+            outputStates.Add(output);
+            
+            return output.toState;
+	    }
+	
+        public AnyState AnyState(string name, Condition switchCondition,
+            EnterState enter = null, UpdateState update = null,
+            FixedUpdateState fixedUpdate = null, ExitState exit = null
+        ) 
+        {
+            return stateMachine.AnyState(name, switchCondition, enter, update, fixedUpdate, exit);
+        }
+	
+        public State GoTo(string name)
+        {
+            return stateMachine.GetState(name);
+        }
+	
+	public State Update(State overrideExit = null) {
+		Enter();
+		
+		if(update != null) {
+			update();
+		}
+		
+		if(overrideExit != null) {
+			Exit();
+			return overrideExit;
+		}
+		
+		for(int i = 0; i < outputStates.Count; ++ i) {
+			if(outputStates[i].condition()) {
+				Exit();
+				return outputStates[i].toState;
+			}
+		}
+		
+		return null;
+	}
+	
+	public void FixedUpdate() {
+		if(fixedUpdate != null) {
+			Enter();
+			fixedUpdate();
+		}
+	}
+	
+}
+
+public class AnyState : State {
+	
+	bool hasExit;
+	
+	// -- //
+	
+	public AnyState(StateMachine stateMachine, string name, EnterState enter, UpdateState update, FixedUpdateState fixedUpdate, ExitState exit)
+		: base(stateMachine, name, enter, update, fixedUpdate, exit)
+	{  }
+	
+	public AnyState Exit(Condition exitCondition) {
+		// TODO si Exit est appelé plus d'une fois, il faudrait remplacer la condition déjà présente
+		
+		// In Update, the first condition to test will be the exit condition
+		hasExit = true;
+		outputStates.Insert(0, new StateOutput() { condition = exitCondition });
+		return this;
+	}
+	
+	public void SetPreviousState(State previousState) {
+		if(hasExit) {
+			outputStates[0].toState = previousState;
+		}
+	}
+	
+}
+
+public class StateMachine {
+	
+	public State CurrentState {
+		get { return currentState; }
+	}
+	
+	// -- //
+	
+	State currentState;
+	Dictionary<string, State> states = new Dictionary<string, State>();
+	List<StateOutput> anyStates      = new List<StateOutput>();
+	
+	// -- //
+	
+	public State AddState(string name, EnterState enter = null,
+			UpdateState update = null, FixedUpdateState fixedUpdate = null,
+			ExitState exit = null
+	) {
+		State newState = new State(this, name, enter, update, fixedUpdate, exit);
+		
+		if(currentState == null) {
+			currentState = newState;
+		}
+		
+		states.Add(name, newState);
+		
+		return newState;
+	}
+	
+	public AnyState AnyState(string name, Condition switchCondition,
+			EnterState enter = null, UpdateState update = null,
+			FixedUpdateState fixedUpdate = null, ExitState exit = null
+	) {
+		AnyState stateToAdd = GetState(name) as AnyState;
+		
+		if(stateToAdd == null) {
+			stateToAdd = new AnyState(this, name, enter, update, fixedUpdate, exit);
+		}
+		
+		StateOutput output = new StateOutput();
+		output.condition   = switchCondition;
+		output.toState     = stateToAdd;
+		
+		anyStates.Add(output);
+		states.Add(name, stateToAdd);
+		
+		return stateToAdd;
+	}
+	
+	public State GetState(string name) {
+		State maybeTheState;
+		
+		if(states.TryGetValue(name, out maybeTheState)) {
+			return maybeTheState;
+		}
+		
+		return null;
+	}
+	
+	public void Update() {
+		State maybeNewState;
+		AnyState maybeAnyState = null;
+		
+		for(int i = 0; i < anyStates.Count; ++ i) {
+			StateOutput output = anyStates[i];
+			if(output.toState != currentState && output.condition()) {
+				maybeAnyState = output.toState as AnyState; 
+				maybeAnyState.SetPreviousState(currentState);
+			}
+		}
+		
+		maybeNewState = currentState.Update(maybeAnyState);
+		
+		if(maybeNewState != null) {
+			currentState = maybeNewState;
+		}
+	}
+	
+	public void FixedUpdate() {
+		currentState.FixedUpdate();
+	}
+	
+}
+
+}
