ff505df60b04af16391bc110f5ef38684e1d42fa
VG <vengadoravg@gmail.com>
2020-05-04 15:58:18 -0400

    fixed a bug on the attack: you could only attack one target at a time

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerAttack : AoeDetector<Attackable> {
+public class PlayerAttack : MonoBehaviour{
   public int damage;
   public float pushBackStrength = 5;
   public Transform pushBackSource;
+  public GameObject attackAoe;
+
+  void OnTriggerEnter (Collider c) {
+    Attackable attackable = c.GetComponentInParent<Attackable>();
+    if (c.gameObject.activeInHierarchy && attackable) {
+      attackable.GetDamage(damage, pushBackSource.position, pushBackStrength);
+    }
+  }
 
   void Update () {
     if (PlayerDecisions.isActive || NpcDialoguePlaceholder.Instance.IsVisible) return;
 
-    if (Input.GetButtonDown("Fire1") && target) {
-      target.GetDamage(damage, pushBackSource.position, pushBackStrength);
+    if (Input.GetButtonDown("Fire1")) {
+      StopAllCoroutines();
+      StartCoroutine(_Attack());
     }
+  }
+
+  IEnumerator _Attack () {
+    attackAoe.SetActive(true);
+    yield return null;
+    attackAoe.SetActive(false);
   }
 }
